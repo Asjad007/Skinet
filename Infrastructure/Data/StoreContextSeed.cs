6a8c6e372d2814b27d9d88a5eb286b7a472283af
Asjad Ali <49393573+Asjad007@users.noreply.github.com>
2024-09-13 02:55:23 +0500

    Seedded data and updated getproducts method

--- Infrastructure/Data/StoreContextSeed.cs
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Infrastructure.Data
 {
-    internal class StoreContextSeed
+    public class StoreContextSeed
     {
+        public static async Task SeedAsync(StoreContext context)
+        {
+            if (!context.Products.Any())
+            {
+
+                var productsData = await File.ReadAllTextAsync("../Infrastructure/Data/SeedData/products.json");
+
+                var product = JsonSerializer.Deserialize<List<Product>>(productsData);
+
+                if (product == null) return;
+
+                context.Products.AddRange(product);
+
+                await context.SaveChangesAsync();
+
+            }
+
+        }
     }
 }
--- skinet.Server/Controllers/ProductsController.cs
+++ skinet.Server/Controllers/ProductsController.cs
@@ -14,9 +14,9 @@ namespace skinet.Server.Controllers
         private readonly IProductRepository repo = repo;
 
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts()
+        public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts(string? brand, string? type, string? sort)
         {
-            return Ok(await repo.GetProductsAsync());
+            return Ok(await repo.GetProductsAsync(brand, type, sort));
         }
 
         [HttpGet("{id:int}")]
@@ -71,6 +71,19 @@ namespace skinet.Server.Controllers
             return BadRequest("Problem deleting the product");
 
         }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult> GetBrands()
+        {
+            return Ok(await repo.GetBrandsAsync());
+        }
+        
+        [HttpGet("types")]
+        public async Task<ActionResult> GetTypes()
+        {
+            return Ok(await repo.GetTypesAsync());
+        }
+
         private bool ProductExists(int id)
         {
             return repo.ProductExists(id);
