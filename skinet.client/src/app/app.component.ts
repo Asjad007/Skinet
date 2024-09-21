7632c8e6eba0fe6e17b158aa59a49f54a33eb564
Asjad Ali <49393573+Asjad007@users.noreply.github.com>
2024-09-22 04:36:08 +0500

    products listing and removed some unnecessary files

--- skinet.client/src/app/app.component.ts
+++ skinet.client/src/app/app.component.ts
@@ -1,14 +1,31 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layout/header/header.component';
 
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  baseUrl = 'https://localhost:5000/api/'
+  private http = inject(HttpClient);
 
   title = 'Skinet';
+
+  ngOnInit(): void {
+
+    this.http.get(this.baseUrl +'products').subscribe({
+      next: data => console.log(data),
+      error: error => console.log(error),
+      complete: () => console.log('complete')
+    })
+  
+  }
+
 }
